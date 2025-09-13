fe6fce9060eeeba5b103f21b5e87eea4248bb8d1
saqibmurtaza <saqibmurtazakhan@gmai.com>
2025-09-13 13:58:43 +0500

    All issues Resolved -App is in working state

--- src/asuno_salon_birmingham/backend/models/booking_models.py
+++ src/asuno_salon_birmingham/backend/models/booking_models.py
@@ -3,19 +3,12 @@ from sqlmodel import SQLModel, Field, Column, String
 from datetime import date, time
 from pydantic import BaseModel
 from sqlalchemy import Boolean
-import uuid
-import base64
 from uuid import UUID, uuid4
-
-def generate_base64_uuid() -> str:
-    u = uuid.uuid4()
-    b64 = base64.urlsafe_b64encode(u.bytes).rstrip(b'=').decode('ascii')
-    return b64
 
 class Booking(SQLModel, table=True):
     __tablename__ = "bookings"
     
-    id: Optional[str] = Field(default_factory=generate_base64_uuid, primary_key=True)
+    id: UUID = Field(default_factory=uuid4, primary_key=True, index=True, nullable=False)
     service: str = Field(sa_column=Column(String, nullable=False))
     category: Optional[str] = Field(default=None, sa_column=Column(String))
     date: date
@@ -39,4 +32,3 @@ class BookingOut(BookingCreate):
 
     class Config:
         orm_mode = True
-
